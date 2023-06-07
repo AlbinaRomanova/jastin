3f75a2da09430c6174304fe99bf760e6469beaf7
AlbinaRomanova <132841443+AlbinaRomanova@users.noreply.github.com>
2023-06-07 10:01:38 +0400

    edits for phone

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function openModal(imageElement) {
-    document.getElementById('myModal').style.display = "block";
-    document.getElementById('img01').src = imageElement.src;
+function openModal (imageElement) {
+  document.getElementById('myModal').style.display = 'block';
+  document.getElementById('img01').src = imageElement.src;
 }
 
-function closeModal() {
-    document.getElementById('myModal').style.display = "none";
+function closeModal () {
+  document.getElementById('myModal').style.display = 'none';
 }
