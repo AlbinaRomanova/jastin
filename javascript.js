8e487147064564641b9844a08d0989f1abb78974
AlbinaRomanova <132841443+AlbinaRomanova@users.noreply.github.com>
2023-06-08 21:11:58 +0400

    player add

--- javascript.js
+++ javascript.js
@@ -1,24 +1,24 @@
-console.log('Скрипт загружен');
+const playerContainer = document.querySelector('.audio-container');
+const dragArea = document.querySelector('.drag-area');
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const audioPlayer = document.querySelector('.audio-player');
-  let isMoving = false;
-  let startX = 0; let startY = 0;
+let isMoving = false;
+let startX = 0;
+let startY = 0;
 
-  audioPlayer.addEventListener('mousedown', (e) => {
-    startX = e.clientX - audioPlayer.offsetLeft;
-    startY = e.clientY - audioPlayer.offsetTop;
-    isMoving = true;
-  });
+dragArea.addEventListener('pointerdown', (e) => {
+  startX = e.clientX - playerContainer.offsetLeft;
+  startY = e.clientY - playerContainer.offsetTop;
+  isMoving = true;
+});
 
-  document.addEventListener('mousemove', (e) => {
-    if (isMoving) {
-      audioPlayer.style.left = (e.clientX - startX) + 'px';
-      audioPlayer.style.top = (e.clientY - startY) + 'px';
-    }
-  });
+window.addEventListener('pointermove', (e) => {
+  if (isMoving) {
+    e.preventDefault();
+    playerContainer.style.left = (e.clientX - startX) + 'px';
+    playerContainer.style.top = (e.clientY - startY) + 'px';
+  }
+});
 
-  document.addEventListener('mouseup', () => {
-    isMoving = false;
-  });
+window.addEventListener('pointerup', () => {
+  isMoving = false;
 });
